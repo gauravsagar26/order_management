268da7fc87e1e5768138916fe51546f21a6d85bc
gauravsagar26 <gauravsagar26@gmail.com>
2025-02-21 11:47:30 +0530

    Fix: Removed custom app duplicate

--- order_management/orders/types.py
+++
@@ -1,9 +0,0 @@
-
-from pydantic import BaseModel
-from typing import List, Optional
-
-class OrderRequestEntity(BaseModel):
-    user_id: int
-    order_id: Optional[int]
-    item_ids: Optional[List[int]]
-    total_amount: Optional[float]
--- order_management/orders/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from orders.views import OrdersView, MetricsViewSet
-
-urlpatterns = [
-    path('orders/', OrdersView.as_view(), name='orders'),
-    path('metrics/', MetricsViewSet.as_view(), name='metrics'),
-]
